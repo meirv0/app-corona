4a96a8ca3f81790de932de2a7ef8ab633f5a6405
Meir Vaikselbom <meirv0@gmail.com>
2020-03-02 02:11:56 +0200

    error handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
 import './services/mongoConnect';
 import reportsRouter from './container/reports/reports.router';
 
@@ -8,5 +8,18 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use('/api/v1/reports', reportsRouter);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+
+    if (res.headersSent) {
+        return;
+    }
+
+    if (!res.statusCode) {
+        res.status(500);
+    }
+
+    res.json({ message: err.message });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
